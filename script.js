553f2ca4d0b80de6fbc13435b38a6fdfc2f25f01
s.xuan.0.6 <156971076+adleyliuu@users.noreply.github.com>
2026-09-08 15:09:28 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const projects = document.querySelectorAll(".project");
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    },
+    {
+        threshold: 0.15
+    }
+);
+
+projects.forEach((project) => {
+    observer.observe(project);
+});
